8795d140ef3195849f9a3d6d4b8e1b85c4914df4
Hyunil Park <hyunil46.park@samsung.com>
2015-01-09 11:21:50 +0900

    Revert "Featuer Enhancement : A Media Player libray API is changed to Tizen2.3 version"
    This reverts commit e797c47808d78c9b802629505012faeb57a75824.
    
    Change-Id: I37d815cbd8b13afcc139e32f87f203022d3ff724
    Signed-off-by: Hyunil Park <hyunil46.park@samsung.com>

--- include/player_private.h
+++ include/player_private.h
@@ -23,28 +23,6 @@
 extern "C" {
 #endif
 
-#ifdef LOG_TAG
-#undef LOG_TAG
-#endif
-#define LOG_TAG "TIZEN_N_PLAYER"
-
-#define PLAYER_CHECK_CONDITION(condition,error,msg)     \
-                if(condition) {} else \
-                { LOGE("[%s] %s(0x%08x)",__FUNCTION__, msg,error); return error;}; \
-
-#define PLAYER_INSTANCE_CHECK(player)   \
-        PLAYER_CHECK_CONDITION(player != NULL, PLAYER_ERROR_INVALID_PARAMETER,"PLAYER_ERROR_INVALID_PARAMETER")
-
-#define PLAYER_STATE_CHECK(player,expected_state)       \
-        PLAYER_CHECK_CONDITION(player->state == expected_state,PLAYER_ERROR_INVALID_STATE,"PLAYER_ERROR_INVALID_STATE")
-
-#define PLAYER_NULL_ARG_CHECK(arg)      \
-        PLAYER_CHECK_CONDITION(arg != NULL,PLAYER_ERROR_INVALID_PARAMETER,"PLAYER_ERROR_INVALID_PARAMETER")
-
-#define PLAYER_RANGE_ARG_CHECK(arg, min, max)      \
-        PLAYER_CHECK_CONDITION(arg <= max,PLAYER_ERROR_INVALID_PARAMETER,"PLAYER_ERROR_INVALID_PARAMETER")		\
-        PLAYER_CHECK_CONDITION(arg >= min,PLAYER_ERROR_INVALID_PARAMETER,"PLAYER_ERROR_INVALID_PARAMETER")
-
 typedef enum {
 	_PLAYER_EVENT_TYPE_PREPARE,
 	_PLAYER_EVENT_TYPE_COMPLETE,
@@ -54,17 +32,13 @@ typedef enum {
 	_PLAYER_EVENT_TYPE_SUBTITLE,
 	_PLAYER_EVENT_TYPE_CAPTURE,
 	_PLAYER_EVENT_TYPE_SEEK,
-	_PLAYER_EVENT_TYPE_MEDIA_PACKET_VIDEO_FRAME,
+	_PLAYER_EVENT_TYPE_VIDEO_FRAME,
 	_PLAYER_EVENT_TYPE_AUDIO_FRAME,
 	_PLAYER_EVENT_TYPE_VIDEO_FRAME_RENDER_ERROR,
 	_PLAYER_EVENT_TYPE_PD,
 	_PLAYER_EVENT_TYPE_SUPPORTED_AUDIO_EFFECT,
 	_PLAYER_EVENT_TYPE_SUPPORTED_AUDIO_EFFECT_PRESET,
 	_PLAYER_EVENT_TYPE_MISSED_PLUGIN,
-#ifdef _PLAYER_FOR_PRODUCT
-	_PLAYER_EVENT_TYPE_IMAGE_BUFFER,
-	_PLAYER_EVENT_TYPE_SELECTED_SUBTITLE_LANGUAGE,
-#endif
 	_PLAYER_EVENT_TYPE_NUM
 }_player_event_e;
 
@@ -80,14 +54,7 @@ typedef struct _player_s{
 	bool is_display_visible;
 	bool is_progressive_download;
 	pthread_t prepare_async_thread;
-	GHashTable *ecore_jobs;
-	player_error_e error_code;
-	bool is_doing_jobs;
-	media_format_h pkt_fmt;
 } player_s;
-
-int __player_convert_error_code(int code, char* func_name);
-bool __player_state_validate(player_s * handle, player_state_e threshold);
 
 #ifdef __cplusplus
 }
