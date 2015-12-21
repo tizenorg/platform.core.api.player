6b903e72e5163ffcc03a09d2fcb6b9bb3ed9c34f
Gilbok Lee <gilbok.lee@samsung.com>
2015-12-21 11:04:20 +0900

    Apply Tizen coding rule and add doc
    Change-Id: Iee8ff8e8b8d2d9c48575ebbbfd87fa0208f639d4
    Signed-off-by: Gilbok Lee <gilbok.lee@samsung.com>

--- src/player_priv.c
+++ src/player_priv.c
@@ -31,15 +31,15 @@
 /*
 * Implementation for mused
 */
-int player_set_shm_stream_path_for_mused (player_h player, const char *stream_path)
+int player_set_shm_stream_path_for_mused(player_h player, const char *stream_path)
 {
 	PLAYER_INSTANCE_CHECK(player);
 	PLAYER_NULL_ARG_CHECK(stream_path);
 
-	player_s * handle = (player_s *) player;
+	player_s *handle = (player_s *)player;
 
-	if(mm_player_set_shm_stream_path(handle->mm_handle, stream_path)
-			!= MM_ERROR_NONE) {
+	if (mm_player_set_shm_stream_path(handle->mm_handle, stream_path)
+		!= MM_ERROR_NONE) {
 		LOGE("Fail to set attribute stream path");
 		return PLAYER_ERROR_INVALID_OPERATION;
 	}
@@ -48,7 +48,7 @@ int player_set_shm_stream_path_for_mused (player_h player, const char *stream_pa
 
 static MMDisplaySurfaceType __player_mused_convet_display_type(player_display_type_e type)
 {
-	switch(type) {
+	switch (type) {
 	case PLAYER_DISPLAY_TYPE_OVERLAY:
 		return MM_DISPLAY_SURFACE_REMOTE;
 #ifdef TIZEN_MOBILE
@@ -57,65 +57,53 @@ static MMDisplaySurfaceType __player_mused_convet_display_type(player_display_ty
 #endif
 	case PLAYER_DISPLAY_TYPE_NONE:
 		return MM_DISPLAY_SURFACE_NULL;
-	default :
+	default:
 		return MM_DISPLAY_SURFACE_NULL;
 	}
 }
 
-
 #ifdef HAVE_WAYLAND
-int player_set_display_wl_for_mused(player_h player, player_display_type_e type, intptr_t surface,
-		int x, int y, int w, int h)
+int player_set_display_wl_for_mused(player_h player, player_display_type_e type, intptr_t surface, int x, int y, int w, int h)
 {
 	PLAYER_INSTANCE_CHECK(player);
-	player_s * handle = (player_s *) player;
+	player_s *handle = (player_s *)player;
 	void *set_handle = NULL;
 	MMDisplaySurfaceType mmType = __player_mused_convet_display_type(type);
 	MMDisplaySurfaceType mmClientType = MM_DISPLAY_SURFACE_NULL;
 	MMPlayerPipelineType mmPipelineType = MM_PLAYER_PIPELINE_SERVER;
 
 	int ret;
-	if (!__player_state_validate(handle, PLAYER_STATE_IDLE))
-	{
-		LOGE("[%s] PLAYER_ERROR_INVALID_STATE(0x%08x) : current state - %d" ,__FUNCTION__,PLAYER_ERROR_INVALID_STATE, handle->state);
+	if (!__player_state_validate(handle, PLAYER_STATE_IDLE)) {
+		LOGE("[%s] PLAYER_ERROR_INVALID_STATE(0x%08x) : current state - %d", __FUNCTION__, PLAYER_ERROR_INVALID_STATE, handle->state);
 		return PLAYER_ERROR_INVALID_STATE;
 	}
-	if (handle->is_set_pixmap_cb)
-	{
-		if (handle->state < PLAYER_STATE_READY)
-		{
+	if (handle->is_set_pixmap_cb) {
+		if (handle->state < PLAYER_STATE_READY) {
 			/* just set below and go to "changing surface case" */
 			handle->is_set_pixmap_cb = false;
-		}
-		else
-		{
-			LOGE("[%s] pixmap callback was set, try it again after calling player_unprepare()" ,__FUNCTION__,PLAYER_ERROR_INVALID_OPERATION);
-			LOGE("[%s] PLAYER_ERROR_INVALID_OPERATION(0x%08x)" ,__FUNCTION__,PLAYER_ERROR_INVALID_OPERATION);
+		} else {
+			LOGE("[%s] pixmap callback was set, try it again after calling player_unprepare()", __FUNCTION__, PLAYER_ERROR_INVALID_OPERATION);
+			LOGE("[%s] PLAYER_ERROR_INVALID_OPERATION(0x%08x)", __FUNCTION__, PLAYER_ERROR_INVALID_OPERATION);
 			return PLAYER_ERROR_INVALID_OPERATION;
 		}
 	}
 
-	void* temp = NULL;
-	if (type == PLAYER_DISPLAY_TYPE_NONE)
-	{
+	void *temp = NULL;
+	if (type == PLAYER_DISPLAY_TYPE_NONE) {
 		/* NULL surface */
 		handle->display_handle = 0;
 		handle->display_type = type;
 		set_handle = NULL;
-	}
-	else
-	{
+	} else {
 		/* get handle from overlay or evas surface */
 		temp = handle->display_handle;
-		if (type == PLAYER_DISPLAY_TYPE_OVERLAY)
-		{
+		if (type == PLAYER_DISPLAY_TYPE_OVERLAY) {
 			LOGI("Wayland overlay surface type");
 			handle->display_handle = (void *)surface;
 			set_handle = &(handle->display_handle);
 			mmClientType = MM_DISPLAY_SURFACE_X;
 #ifdef TIZEN_MOBILE
-		} else if (type == PLAYER_DISPLAY_TYPE_EVAS)
-		{
+		} else if (type == PLAYER_DISPLAY_TYPE_EVAS) {
 			LOGI("Evas surface type");
 			handle->display_handle = (void *)surface;
 			set_handle = &(handle->display_handle);
@@ -128,77 +116,49 @@ int player_set_display_wl_for_mused(player_h player, player_display_type_e type,
 	}
 
 	/* set display handle */
-	if (handle->display_type == PLAYER_DISPLAY_TYPE_NONE || type == handle->display_type) // first time or same type
-	{
+	if (handle->display_type == PLAYER_DISPLAY_TYPE_NONE || type == handle->display_type) {
+		/* first time or same type */
 		LOGW("first time or same type");
-		ret = mm_player_set_attribute(handle->mm_handle, NULL,
-			"display_surface_type", mmType,
-			"display_surface_client_type", mmClientType,
-			"display_overlay", set_handle, sizeof(void *),
-			"pipeline_type", mmPipelineType,
-			NULL);
+		ret = mm_player_set_attribute(handle->mm_handle, NULL, "display_surface_type", mmType, "display_surface_client_type", mmClientType, "display_overlay", set_handle, sizeof(void *), "pipeline_type", mmPipelineType, NULL);
 
-		if (ret != MM_ERROR_NONE)
-		{
+		if (ret != MM_ERROR_NONE) {
 			handle->display_handle = temp;
-			LOGE("[%s] Failed to display surface change :%d",__FUNCTION__,ret);
-		}
-		else
-		{
-			if (type != PLAYER_DISPLAY_TYPE_NONE)
-			{
+			LOGE("[%s] Failed to display surface change :%d", __FUNCTION__, ret);
+		} else {
+			if (type != PLAYER_DISPLAY_TYPE_NONE) {
 				handle->display_type = type;
-				LOGI("[%s] video display has been changed- type :%d, addr : 0x%x",
-					__FUNCTION__,handle->display_type, handle->display_handle);
-			}
-			else
+				LOGI("[%s] video display has been changed- type :%d, addr : 0x%x", __FUNCTION__, handle->display_type, handle->display_handle);
+			} else
 				LOGI("NULL surface");
 		}
 		LOGI("get window rectangle: x(%d) y(%d) width(%d) height(%d)", x, y, w, h);
-		ret = mm_player_set_attribute(handle->mm_handle, NULL,
-			"wl_window_render_x", x,
-			"wl_window_render_y", y,
-			"wl_window_render_width", w,
-			"wl_window_render_height", h,
-			(char*)NULL);
+		ret = mm_player_set_attribute(handle->mm_handle, NULL, "wl_window_render_x", x, "wl_window_render_y", y, "wl_window_render_width", w, "wl_window_render_height", h, (char *)NULL);
 
-		if (ret != MM_ERROR_NONE)
-		{
+		if (ret != MM_ERROR_NONE) {
 			handle->display_handle = temp;
-			LOGE("[%s] Failed to set wl_window render rectangle :%d",__FUNCTION__,ret);
+			LOGE("[%s] Failed to set wl_window render rectangle :%d", __FUNCTION__, ret);
 		}
-	}
-	else //changing surface case
-	{
+	} else {
+		/* changing surface case */
 		ret = mm_player_change_videosink(handle->mm_handle, mmType, set_handle);
-		if (ret != MM_ERROR_NONE)
-		{
+		if (ret != MM_ERROR_NONE) {
 			handle->display_handle = temp;
-			if(ret == MM_ERROR_NOT_SUPPORT_API)
-			{
-				LOGE("[%s] change video sink is not available.",__FUNCTION__);
+			if (ret == MM_ERROR_NOT_SUPPORT_API) {
+				LOGE("[%s] change video sink is not available.", __FUNCTION__);
 				ret = PLAYER_ERROR_NONE;
+			} else {
+				LOGE("[%s] Failed to display surface change :%d", __FUNCTION__, ret);
 			}
-			else
-			{
-				LOGE("[%s] Failed to display surface change :%d",__FUNCTION__,ret);
-			}
-		}
-		else
-		{
+		} else {
 			handle->display_type = type;
-			LOGI("[%s] video display has been changed- type :%d, addr : 0x%x",
-				__FUNCTION__,handle->display_type, handle->display_handle);
+			LOGI("[%s] video display has been changed- type :%d, addr : 0x%x", __FUNCTION__, handle->display_type, handle->display_handle);
 		}
 	}
 
-	if(ret != MM_ERROR_NONE)
-	{
+	if (ret != MM_ERROR_NONE) {
 		handle->display_type = PLAYER_DISPLAY_TYPE_NONE;
-		return __player_convert_error_code(ret,(char*)__FUNCTION__);
-	}
-	else
-	{
+		return __player_convert_error_code(ret, (char *)__FUNCTION__);
+	} else {
 		return PLAYER_ERROR_NONE;
 	}
 }
@@ -208,135 +168,100 @@ int player_set_display_wl_for_mused(player_h player, player_display_type_e type,
 int player_set_display_for_mused(player_h player, player_display_type_e type, unsigned int xhandle)
 {
 	PLAYER_INSTANCE_CHECK(player);
-	player_s * handle = (player_s *) player;
+	player_s *handle = (player_s *)player;
 	void *set_handle = NULL;
 	MMDisplaySurfaceType mmType = __player_mused_convet_display_type(type);
 
 	int ret;
-	if (!__player_state_validate(handle, PLAYER_STATE_IDLE))
-	{
-		LOGE("[%s] PLAYER_ERROR_INVALID_STATE(0x%08x) : current state - %d" ,__FUNCTION__,PLAYER_ERROR_INVALID_STATE, handle->state);
+	if (!__player_state_validate(handle, PLAYER_STATE_IDLE)) {
+		LOGE("[%s] PLAYER_ERROR_INVALID_STATE(0x%08x) : current state - %d", __FUNCTION__, PLAYER_ERROR_INVALID_STATE, handle->state);
 		return PLAYER_ERROR_INVALID_STATE;
 	}
 
-	if (handle->is_set_pixmap_cb)
-	{
-		if (handle->state < PLAYER_STATE_READY)
-		{
+	if (handle->is_set_pixmap_cb) {
+		if (handle->state < PLAYER_STATE_READY) {
 			/* just set below and go to "changing surface case" */
 			handle->is_set_pixmap_cb = false;
-		}
-		else
-		{
-			LOGE("[%s] pixmap callback was set, try it again after calling player_unprepare()" ,__FUNCTION__,PLAYER_ERROR_INVALID_OPERATION);
-			LOGE("[%s] PLAYER_ERROR_INVALID_OPERATION(0x%08x)" ,__FUNCTION__,PLAYER_ERROR_INVALID_OPERATION);
+		} else {
+			LOGE("[%s] pixmap callback was set, try it again after calling player_unprepare()", __FUNCTION__, PLAYER_ERROR_INVALID_OPERATION);
+			LOGE("[%s] PLAYER_ERROR_INVALID_OPERATION(0x%08x)", __FUNCTION__, PLAYER_ERROR_INVALID_OPERATION);
 			return PLAYER_ERROR_INVALID_OPERATION;
 		}
 	}
 
-	void* temp = NULL;
-	if (type == PLAYER_DISPLAY_TYPE_NONE)
-	{
+	void *temp = NULL;
+	if (type == PLAYER_DISPLAY_TYPE_NONE) {
 		/* NULL surface */
 		handle->display_handle = 0;
 		handle->display_type = PLAYER_DISPLAY_TYPE_NONE;
 		set_handle = NULL;
-	}
-	else
-	{
+	} else {
 		/* get handle from overlay or evas surface */
 		temp = handle->display_handle;
-		if (type == PLAYER_DISPLAY_TYPE_OVERLAY /*&& !strcmp(object_type, "elm_win")*/)
-		{
+		if (type == PLAYER_DISPLAY_TYPE_OVERLAY/* && !strcmp(object_type, "elm_win") */) {
 			/* x window overlay surface */
 			LOGI("overlay surface type");
 			handle->display_handle = (void *)(uintptr_t)xhandle;
 			set_handle = &(handle->display_handle);
-		}
-		else
-		{
+		} else {
 			LOGE("invalid surface type");
 			return PLAYER_ERROR_INVALID_PARAMETER;
 		}
 	}
 
 	/* set display handle */
-	if (handle->display_type == PLAYER_DISPLAY_TYPE_NONE || type == handle->display_type) // first time or same type
-	{
-		ret = mm_player_set_attribute(handle->mm_handle, NULL,
-			"display_surface_type", mmType,
-			"display_overlay", set_handle, sizeof(xhandle),
-			NULL);
+	if (handle->display_type == PLAYER_DISPLAY_TYPE_NONE || type == handle->display_type) {
+		/* first time or same type */
+		ret = mm_player_set_attribute(handle->mm_handle, NULL, "display_surface_type", mmType, "display_overlay", set_handle, sizeof(xhandle), NULL);
 
-		if (ret != MM_ERROR_NONE)
-		{
+		if (ret != MM_ERROR_NONE) {
 			handle->display_handle = temp;
-			LOGE("[%s] Failed to display surface change :%d",__FUNCTION__,ret);
-		}
-		else
-		{
-			if (type != PLAYER_DISPLAY_TYPE_NONE)
-			{
+			LOGE("[%s] Failed to display surface change :%d", __FUNCTION__, ret);
+		} else {
+			if (type != PLAYER_DISPLAY_TYPE_NONE) {
 				handle->display_type = type;
-				LOGI("[%s] video display has been changed- type :%d, addr : 0x%x",
-					__FUNCTION__,handle->display_type, handle->display_handle);
-			}
-			else
+				LOGI("[%s] video display has been changed- type :%d, addr : 0x%x", __FUNCTION__, handle->display_type, handle->display_handle);
+			} else
 				LOGI("NULL surface");
 		}
-	}
-	else //changing surface case
-	{
+	} else {
+		/* changing surface case */
 		ret = mm_player_change_videosink(handle->mm_handle, mmType, set_handle);
-		if (ret != MM_ERROR_NONE)
-		{
+		if (ret != MM_ERROR_NONE) {
 			handle->display_handle = temp;
-			if(ret == MM_ERROR_NOT_SUPPORT_API)
-			{
-				LOGE("[%s] change video sink is not available.",__FUNCTION__);
+			if (ret == MM_ERROR_NOT_SUPPORT_API) {
+				LOGE("[%s] change video sink is not available.", __FUNCTION__);
 				ret = PLAYER_ERROR_NONE;
+			} else {
+				LOGE("[%s] Failed to display surface change :%d", __FUNCTION__, ret);
 			}
-			else
-			{
-				LOGE("[%s] Failed to display surface change :%d",__FUNCTION__,ret);
-			}
-		}
-		else
-		{
+		} else {
 			handle->display_type = type;
-			LOGI("[%s] video display has been changed- type :%d, addr : 0x%x",
-				__FUNCTION__,handle->display_type, handle->display_handle);
+			LOGI("[%s] video display has been changed- type :%d, addr : 0x%x", __FUNCTION__, handle->display_type, handle->display_handle);
 		}
 	}
 
-	if(ret != MM_ERROR_NONE)
-	{
+	if (ret != MM_ERROR_NONE) {
 		handle->display_type = PLAYER_DISPLAY_TYPE_NONE;
-		return __player_convert_error_code(ret,(char*)__FUNCTION__);
-	}
-	else
-	{
+		return __player_convert_error_code(ret, (char *)__FUNCTION__);
+	} else {
 		return PLAYER_ERROR_NONE;
 	}
 }
 #endif
 
-int player_set_audio_policy_info_for_mused(player_h player,
-	char *stream_type, int stream_index)
+int player_set_audio_policy_info_for_mused(player_h player, char *stream_type, int stream_index)
 {
 	PLAYER_INSTANCE_CHECK(player);
-	player_s * handle = (player_s *) player;
+	player_s *handle = (player_s *)player;
 	int ret;
 
 	PLAYER_STATE_CHECK(handle, PLAYER_STATE_IDLE);
 
-	ret = mm_player_set_attribute(handle->mm_handle, NULL,
-			"sound_stream_type", stream_type, strlen(stream_type),
-			"sound_stream_index", stream_index,
-			NULL);
+	ret = mm_player_set_attribute(handle->mm_handle, NULL, "sound_stream_type", stream_type, strlen(stream_type), "sound_stream_index", stream_index, NULL);
 
-	if(ret != MM_ERROR_NONE)
-		return __player_convert_error_code(ret,(char*)__FUNCTION__);
+	if (ret != MM_ERROR_NONE)
+		return __player_convert_error_code(ret, (char *)__FUNCTION__);
 	else
 		return PLAYER_ERROR_NONE;
 }
@@ -346,12 +271,11 @@ int player_get_raw_video_caps(player_h player, char **caps)
 	int ret;
 	PLAYER_INSTANCE_CHECK(player);
 	PLAYER_NULL_ARG_CHECK(caps);
-	player_s * handle = (player_s *) player;
+	player_s *handle = (player_s *)player;
 
 	ret = mm_player_get_raw_video_caps(handle->mm_handle, caps);
-	if(ret != MM_ERROR_NONE) {
-		return __player_convert_error_code(ret,(char*)__FUNCTION__);
-	}
+	if (ret != MM_ERROR_NONE)
+		return __player_convert_error_code(ret, (char *)__FUNCTION__);
 
 	return PLAYER_ERROR_NONE;
 }
@@ -362,36 +286,36 @@ int player_set_video_bin_created_cb(player_h player, player_video_bin_created_cb
 	PLAYER_NULL_ARG_CHECK(callback);
 	int type = _PLAYER_EVENT_TYPE_VIDEO_BIN_CREATED;
 
-	player_s * handle = (player_s *) player;
+	player_s *handle = (player_s *)player;
 	handle->user_cb[type] = callback;
 	handle->user_data[type] = user_data;
-	LOGI("[%s] Event type : %d ",__FUNCTION__, type);
+	LOGI("[%s] Event type : %d ", __FUNCTION__, type);
 	return PLAYER_ERROR_NONE;
 }
 
 int player_unset_video_bin_created_cb(player_h player)
 {
 	PLAYER_INSTANCE_CHECK(player);
-	player_s * handle = (player_s *) player;
+	player_s *handle = (player_s *)player;
 	int type = _PLAYER_EVENT_TYPE_VIDEO_BIN_CREATED;
 
 	handle->user_cb[type] = NULL;
 	handle->user_data[type] = NULL;
-	LOGI("[%s] Event type : %d ",__FUNCTION__, type);
+	LOGI("[%s] Event type : %d ", __FUNCTION__, type);
 	return PLAYER_ERROR_NONE;
 }
 
 int player_sound_register(player_h player, int pid)
 {
 	PLAYER_INSTANCE_CHECK(player);
-	player_s * handle = (player_s *) player;
+	player_s *handle = (player_s *)player;
 	int ret;
 
 	PLAYER_STATE_CHECK(handle, PLAYER_STATE_IDLE);
 
 	ret = mm_player_sound_register(handle->mm_handle, pid);
-	if(ret != MM_ERROR_NONE)
-		return __player_convert_error_code(ret,(char*)__FUNCTION__);
+	if (ret != MM_ERROR_NONE)
+		return __player_convert_error_code(ret, (char *)__FUNCTION__);
 
 	return PLAYER_ERROR_NONE;
 }
@@ -399,10 +323,10 @@ int player_sound_register(player_h player, int pid)
 int player_is_streaming(player_h player, bool *is_streaming)
 {
 	PLAYER_INSTANCE_CHECK(player);
-	player_s * handle = (player_s *) player;
+	player_s *handle = (player_s *)player;
 
 	int ret = mm_player_is_streaming(handle->mm_handle, is_streaming);
-	if(ret != MM_ERROR_NONE)
-		return __player_convert_error_code(ret,(char*)__FUNCTION__);
+	if (ret != MM_ERROR_NONE)
+		return __player_convert_error_code(ret, (char *)__FUNCTION__);
 	return PLAYER_ERROR_NONE;
 }
