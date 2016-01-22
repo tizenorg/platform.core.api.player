4e30cee7f38e54003ae8a2f49cd3a33f108f3bb5
Eunhae Choi <eunhae1.choi@samsung.com>
2016-01-22 15:01:05 +0900

    player: add new error return value of push stream api
    Change-Id: I8e19d2548b65edf5fd4b2f4409a31a0fa2803e43

--- include/mobile/player.h
+++ include/mobile/player.h
@@ -84,6 +84,8 @@ typedef enum {
     PLAYER_ERROR_RESOURCE_LIMIT     = PLAYER_ERROR_CLASS | 0x0c,            /**< Resource limit */
     PLAYER_ERROR_PERMISSION_DENIED  = TIZEN_ERROR_PERMISSION_DENIED,        /**< Permission denied */
     PLAYER_ERROR_SERVICE_DISCONNECTED = PLAYER_ERROR_CLASS | 0x0d,          /**< Socket connection lost (Since 3.0) */
+    PLAYER_ERROR_BUFFER_SPACE         = TIZEN_ERROR_BUFFER_SPACE,           /**< No buffer space available (Since 3.0)*/
+
 } player_error_e;
 
 /**
@@ -915,6 +917,8 @@ int player_unset_media_packet_video_frame_decoded_cb(player_h player);
  * @brief  Pushes elementary stream to decode audio or video
  * @since_tizen 2.4
  * @remarks player_set_media_stream_info() should be called before using this API.
+ * @remarks The available buffer size can be set by calling player_set_media_stream_buffer_max_size() API.
+ *          If there is no available buffer space, this api will return error since 3.0.
  * @param[in]  player   The handle to media player
  * @param[in]  packet   The media packet to decode
  * @return @c 0 on success,
@@ -923,8 +927,10 @@ int player_unset_media_packet_video_frame_decoded_cb(player_h player);
  * @retval #PLAYER_ERROR_INVALID_PARAMETER Invalid parameter
  * @retval #PLAYER_ERROR_INVALID_STATE Invalid state
  * @retval #PLAYER_ERROR_NOT_SUPPORTED_FILE File not supported
- * @pre	The player state must be set to #PLAYER_STATE_IDLE at least.
- * @see  player_set_media_stream_info()
+ * @retval #PLAYER_ERROR_BUFFER_SPACE No buffer space available (since 3.0)
+ * @pre The player state must be set to #PLAYER_STATE_IDLE at least.
+ * @see player_set_media_stream_info()
+ * @see player_set_media_stream_buffer_max_size()
  */
 int player_push_media_stream(player_h player, media_packet_h packet);
 
@@ -1025,7 +1031,7 @@ int player_unset_media_stream_seek_cb(player_h player, player_stream_type_e type
  * @remarks If the buffer level over the max size, player_media_stream_buffer_status_cb() will be invoked with overflow status.
  * @param[in] player The handle to the media player
  * @param[in] type   The type of target stream
- * @param[in] max_size The max bytes of buffer
+ * @param[in] max_size The max bytes of buffer, it has to be bigger than zero. (default: 200000)
  * @return @c 0 on success,
  *         otherwise a negative error value
  * @retval #PLAYER_ERROR_NONE Successful
@@ -1063,7 +1069,7 @@ int player_get_media_stream_buffer_max_size(player_h player, player_stream_type_
  * @remarks If the buffer level drops below the percent value, player_media_stream_buffer_status_cb() will be invoked with underrun status.
  * @param[in] player The handle to the media player
  * @param[in] type   The type of target stream
- * @param[in] percent The minimum threshold(0~100) of buffer
+ * @param[in] percent The minimum threshold(0~100) of buffer (default: 0)
  * @return @c 0 on success,
  *         otherwise a negative error value
  * @retval #PLAYER_ERROR_NONE Successful
@@ -1972,3 +1978,4 @@ int player_select_track(player_h player, player_stream_type_e type, int index);
 #endif
 
 #endif /* __TIZEN_MEDIA_PLAYER_H__ */
+
